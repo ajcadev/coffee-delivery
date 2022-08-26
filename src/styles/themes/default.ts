041b135ddf37eb4d185250d2af20efa2aec0663e
Cardoso <cardosoassuncao@gmail.com>
2022-08-26 16:20:46 -0400

    add theme

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+  'base-white': '#FFF',
+  'base-background': '#FAFAFA',
+
+  'base-title': '#272221',
+  'base-subtitle': '#403937',
+  'base-text': '#574F4D',
+  'base-label': '#8D8686',
+  'base-hover': '#D7D5D5',
+  'base-button': '#E6E5E5',
+  'base-input': '#EDEDED',
+  'base-card': '#F3F2F2',
+
+  'brand-yellow-light': '#F1E9C9',
+  'brand-yellow': '#DBAC2C',
+  'brand-yellow-dark': '#C47F17',
+
+  'brand-purple-light': '#EBE5F9',
+  'brand-purple': '#8047F8',
+  'brand-purple-dark': '#4B2995',
+} as const
