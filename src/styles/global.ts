2c4b44d5e66a437e0d143fd15cec43eb52b22cc6
Cardoso <cardosoassuncao@gmail.com>
2022-08-26 16:31:29 -0400

    add global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+*, *::before, *::after {
+  box-sizing: border-box;
+}
+
+* {
+  margin: 0;
+}
+
+html, body, #root {
+  height: 100%;
+}
+
+body {
+  line-height: 1.5;
+  -webkit-font-smoothing: antialiased;
+
+  background-color: ${(props) => props.theme['base-background']};
+  color: ${(props) => props.theme['base-text']};
+}
+
+img, picture, video, canvas, svg {
+  display: block;
+  max-width: 100%;
+}
+
+input, button, textarea, select {
+  font: inherit;
+}
+
+p, h1, h2, h3, h4, h5, h6 {
+  overflow-wrap: break-word;
+}
+
+#root {
+  isolation: isolate;
+}
+
+`
